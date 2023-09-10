9d8b19f53d65ad5d1b66525538a5cc5e97ab0170
EddieCarbon <tomiczekkuba01@gmail.com>
2023-09-11 00:08:55 +0200

    Finished DepartmentController

--- WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/Controllers/DepartmentController.cs
@@ -1,6 +1,9 @@
-﻿using Application.Dto;
+﻿using System.Net;
+using Application.Commands.Departments.DeleteDepartment;
 using Application.Dto.Student;
-using Application.Services.Abstractions;
+using Application.Queries.Departments.GetAllDepartments;
+using Application.Queries.Departments.GetDepartmentById;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -10,55 +13,108 @@ namespace WebAPI.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
-        private readonly IDepartmentService _departmentService;
-
-        public DepartmentController(IDepartmentService departmentService)
+        private readonly IMediator _mediator;
+        
+        public DepartmentController(IMediator mediator)
         {
-            _departmentService = departmentService;
+            _mediator = mediator;
         }
-
+        
         [SwaggerOperation(Summary = "Retrieves all department")]
         [HttpGet]
-        public IActionResult Get()
+        [ProducesResponseType(typeof(ListDepartmentsDto), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get()
         {
-            var departments = _departmentService.GetAllDepartments();
-            return Ok(departments);
+            var result = await _mediator.Send(new GetAllDepartmentsQuery());
+            return Ok(result);
         }
-
+        
         [SwaggerOperation(Summary = "Retrieves a specific department by unique ID")]
         [HttpGet("{Id}")]
-        public IActionResult Get(int DepartmentId)
+        [ProducesResponseType(typeof(DepartmentDetailDto), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get(int Id)
         {
-            var department = _departmentService.GetDepartmentById(DepartmentId);
-
-            if (department is null)
-                return NotFound();
-
-            return Ok(department);
+            var result = await _mediator.Send(new GetDepartmentByIdQuery(Id));
+            return result != null ? Ok(result) : NotFound();
         }
-
+        
         [SwaggerOperation(Summary = "Create a new department")]
         [HttpPost]
-        public IActionResult Create(CreateDepartmentDto newDepartment)
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> Create(CreateDepartmentDto newDepartment)
         {
-            var department = _departmentService.CreateDepartment(newDepartment);
-            return Created($"api/students/{department.DepartmentId}", department);
+            var result = await _mediator.Send(newDepartment);
+            return CreatedAtAction(nameof(Get), new { id = result.DepartmentId }, result);
         }
-
+        
         [SwaggerOperation(Summary = "Update a existing department")]
         [HttpPut]
-        public IActionResult Update(UpdateDepartmentDto updateDepartment)
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Update(UpdateDepartmentDto updateDepartment)
         {
-            _departmentService.UpdateDepartment(updateDepartment);
+            await _mediator.Send(updateDepartment);
+            return NoContent();
+        }
+        
+        [SwaggerOperation(Summary = "Delete a specific department")]
+        [HttpDelete("{Id}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            await _mediator.Send(new DeleteDepartmentCommand(Id));
             return NoContent();
         }
 
-        [SwaggerOperation(Summary = "Delete a specific department")]
-        [HttpDelete("{Id}")]
-        public IActionResult Delete(int DepartmentId)
-        {
-            _departmentService.DeleteDepartment(DepartmentId);
-            return NoContent();
-        }
+        #region Old Solution
+        
+        // private readonly IDepartmentService _departmentService;
+        //
+        // public DepartmentController(IDepartmentService departmentService)
+        // {
+        //     _departmentService = departmentService;
+        // }
+        
+        // [SwaggerOperation(Summary = "Retrieves all department")]
+        // [HttpGet]
+        // public IActionResult Get()
+        // {
+        //     var departments = _departmentService.GetAllDepartments();
+        //     return Ok(departments);
+        // }
+        //
+        // [SwaggerOperation(Summary = "Retrieves a specific department by unique ID")]
+        // [HttpGet("{Id}")]
+        // public IActionResult Get(int DepartmentId)
+        // {
+        //     var department = _departmentService.GetDepartmentById(DepartmentId);
+        //
+        //     if (department is null)
+        //         return NotFound();
+        //
+        //     return Ok(department);
+        // }
+        // [SwaggerOperation(Summary = "Create a new department")]
+        // [HttpPost]
+        // public IActionResult Create(CreateDepartmentDto newDepartment)
+        // {
+        //     var department = _departmentService.CreateDepartment(newDepartment);
+        //     return Created($"api/students/{department.DepartmentId}", department);
+        // }
+        // [SwaggerOperation(Summary = "Update a existing department")]
+        // [HttpPut]
+        // public IActionResult Update(UpdateDepartmentDto updateDepartment)
+        // {
+        //     _departmentService.UpdateDepartment(updateDepartment);
+        //     return NoContent();
+        // }
+        // [SwaggerOperation(Summary = "Delete a specific department")]
+        // [HttpDelete("{Id}")]
+        // public IActionResult Delete(int DepartmentId)
+        // {
+        //     _departmentService.DeleteDepartment(DepartmentId);
+        //     return NoContent();
+        // }
+
+        #endregion
     }
 }
