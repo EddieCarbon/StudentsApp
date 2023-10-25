919caaa4ce27122a6c0de5830035e1bd5a9fa808
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2023-10-25 13:02:55 +0200

    validate errors

---
+++ StudentsApp.Client/Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsApp.Client.Models
+{
+    public class ApiResponse
+    {
+        public bool Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ StudentsApp.Client/Models/Reset.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentsApp.Client.Models
+{
+    public class Reset
+    {
+        public string Token { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
